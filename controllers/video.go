028b8284937d57d4845c49676b5971b99a6b97a2
uestc-wxy <1939311091@qq.com>
2023-08-07 12:22:47 +0800

    初始化

---
+++ controllers/video.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "github.com/beego/beego/v2/server/web"
+
+type VideoController struct {
+	web.Controller
+}
---
+++ models/message.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Message struct {
+	Id         int       `orm:"column(id);pk;auto" description:"消息id" json:"id"`
+	FromUserId *User     `orm:"column(from_user_id);rel(fk)" description:"消息发送者id" json:"from_user_id"`
+	ToUserId   *User     `orm:"column(to_user_id);rel(fk)" description:"消息接收者id" json:"to_user_id"`
+	Content    string    `orm:"column(content);size(1024)" description:"消息内容" json:"content"`
+	CreateTime time.Time `orm:"column(create_time);auto_now_add;type(datetime)" description:"创建时间" json:"create_time"`
+}
