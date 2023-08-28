1934055b2d4d120a24fccf0ee06f3eeccfdd1660
珩 <1939311091@qq.com>
2023-08-28 09:43:44 +0800

    优化

--- app/gateway/wrapper/relation.go
+++ app/gateway/wrapper/relation.go
@@ -3,7 +3,7 @@ package wrapper
 import "github.com/afex/hystrix-go/hystrix"
 
 var ActionRelationFuseConfig = hystrix.CommandConfig{
-	Timeout:                5000,
+	Timeout:                1000,
 	RequestVolumeThreshold: 5000, // 10秒内的请求量，默认值是20，如果超过20那么就判断是否熔断
 	ErrorPercentThreshold:  50,   // 错误百分比，当错误超过百分比时，直接进行降级处理，直至熔断器再次 开启，默认50%
 	SleepWindow:            5000, // 过多长时间，熔断器再次检测是否开启，单位毫秒ms（默认5秒）
