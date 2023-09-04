229e5c49b48c0f8791eb568842973a139e4a0766
david <2508808197@qq.com>
2023-09-05 01:40:30 +0800

    upd

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /usr/local/bin/ &&./etcd &
 cd /usr/local/bin/ &&./jaeger-all-in-one --collector.zipkin.host-port=:9411 &
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server &
 systemctl start redis-server
 
 ./gateway &
